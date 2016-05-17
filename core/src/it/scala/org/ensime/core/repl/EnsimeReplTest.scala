7d001a72dece744f89b2b7c50a0e4be3663ba740
Alex Olkhovskiy <alex@olkhovskiy.com>
2016-05-17 20:47:00 +0100

    wip

---
+++ core/src/it/scala/org/ensime/core/repl/EnsimeReplTest.scala
@@ -0,0 +1,414 @@
+// Copyright: 2010 - 2016 https://github.com/ensime/ensime-server/graphs
+// Licence: http://www.gnu.org/licenses/gpl-3.0.en.html
+package org.ensime.core.repl
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.tools.nsc.interpreter.Results.Result
+import scala.tools.nsc.interpreter.Results.Success
+
+import scala.concurrent.Future
+import scala.concurrent.Await
+import scala.util.Try
+//import org.junit.Assert
+import scala.concurrent.Promise
+import scala.concurrent.duration.DurationInt
+//import org.scalatest._
+//import org.scalatest.Assertions._
+import org.ensime.util.EnsimeSpec
+//import org.scalatest.Assertions
+
+// import org.apache.log4j.BasicConfigurator
+// import org.junit.Assert.assertEquals
+// import org.junit.Ignore
+// import org.junit.Test
+//import org.scalaide.core.internal.repl.EnsimeRepl
+//import org.scalaide.core.internal.repl.EnsimeRepl._
+
+import EnsimeReplTest._
+import EnsimeRepl._
+
+import org.scalatest._
+
+// This test is not a regular part of org.scalaide.core.TestsSuite because
+// when running under Maven the environment isn't set up quite right for the
+// Scala Interpreter's special ClassLoader. At least that's my best guess as
+// to the problem so far. Under Maven {new IMain} throws with the message:
+
+// Failed to initialize compiler: object scala not found.
+// ** Note that as of 2.8 scala does not assume use of the java classpath.
+// ** For the old behavior pass -usejavacp to scala, or if using a Settings
+// ** object programatically, settings.usejavacp.value = true.
+
+// Note that the suggested setting does not solve the problem. All the other
+// tests pass under Maven. That finicky test passes when you set up Eclipse and
+// run it from there. There is another benefit to this approach: Maven isn't
+// currently configured to do code coverage analysis of the unit tests. Eclemma
+// was quite helpful in writing these tests (warning: Eclemma can't deal with a
+// def inside another def).
+
+// Here's an easy way to set up Eclipse: Create a new Scala project. Unzip the
+// Scala compiler plugin into a folder there and add the scala-compiler.jar to
+// the build path (and scala-compiler-src.jar as source attachment). Create a
+// scala.tools.eclipse.interpreter package and copy this file and the
+// EnsimeRepl source code file to there.
+
+// ----------
+
+// capitalization is important...
+// in the EnsimeRepl public API:
+//  - capitalized {Init,Stop,Exec,Drop,Quit} are message/types sent to the Future
+//  - lowercase {starting,...,unknown} are listener methods on the Client
+// in this file we need to write test input and expected output values:
+//  - lowercase {init,stop,exec,drop,quit} are inputs for the Future functions
+//  - capitalized {Starting,...,Unknown} are expected for the Client methods
+
+class EnsimeReplSpec extends EnsimeSpec
+    with EnsimeReplTestUtils {
+  //BasicConfigurator.configure()
+  val helloWorld = InOut("println(\"hello world\")", _ => "hello world\n")
+  val onePlusOne = InOut("1+1", "res" + _ + ": Int = 2\n")
+
+  def allTransitions: Seq[Expect] = // start in Z, go thru every possible path
+    Expect(drop, stop) /*Z*/ ++ execAdded("1") /*H*/ ++ Expect(stop) ++
+      execAdded("2") ++ Expect(drop, Dropped) /*Z*/ ++ initStarted /*R*/ ++
+      Expect(drop, stop, Stopped) /*Z*/ ++ initStarted /*R*/ ++ Expect(Stopped) ++
+      initStarted ++ helloWorld.execDone(0) /*B*/ ++ Expect(stop, Stopped) /*H*/ ++
+      onePlusOne.execAdded ++ initReplay(helloWorld, onePlusOne) /*B*/ ++
+      onePlusOne.execDone(2) ++ Expect(Stopped) ++
+      initReplay(helloWorld, onePlusOne, onePlusOne) ++ Expect(drop, Dropped) /*R*/ ++
+      onePlusOne.execDone(3) /*B*/ ++ onePlusOne.execDone(4) ++ Expect(Stopped) ++
+      initReplay(onePlusOne, onePlusOne) ++ quitFromB
+
+  def failures1: Seq[Expect] =
+    Echopreter.steal(initStarted /*R*/ ++ onePlusOne.execDone(0) /*B*/ ) ++
+      Expect(Stopped) ++ Failder.initFailed /*H*/ ++ quitFromH
+  def failures2: Seq[Expect] =
+    onePlusOne.execAdded ++ Failpreter.initReplayingFailed(onePlusOne) /*B*/ ++
+      quitFromB
+  def failures3: Seq[Expect] =
+    Failpreter.initStarted /*R*/ ++ helloWorld.execFailed ++ quitFromB
+  def failures: Seq[Seq[Expect]] =
+    Seq(failures1, failures2, failures3) map Echopreter.editOutput
+
+  def unknowns: Seq[Expect] = { // some request messages that aren't recognized
+    val anys = Seq[Any](1, 0.0, true, 'J', null, new Object, new Throwable)
+    val unkns = (anys map { a => Expect(bad(a), Unknown(a)) }).flatten ++ quitFromZ
+    unkns
+  }
+
+  def multiple(n: Int): Seq[Seq[Expect]] = { // all the above at once
+    val tests = failures ++ failures ++ failures ++ // cheap "load balancing"
+      (Seq(allTransitions, unknowns, unknowns, unknowns) map Echopreter.steal)
+    val stream = Stream.continually(tests.toStream).flatten
+    stream.take(n * tests.size).toSeq
+  }
+
+  // start with the Echopreter testing the state machine ...
+  it should "do something that I don't understand yet" in {
+    test(Echopreter.steal(allTransitions))
+  }
+
+  // @Test def allTransitions_Echopreter(): Unit = {
+  //   test(Echopreter.steal(allTransitions))
+  // }
+
+  // @Test def failures_Failpreter(): Unit = {
+  //   test(failures: _*)
+  // }
+
+  // @Test def unknowns_Echopreter(): Unit = {
+  //   test(Echopreter.steal(unknowns))
+  // }
+
+  // // next use multiple EnsimeRepls in parallel to test the Future stuff ...
+
+  // @Test def multiple_RTPScheduler(): Unit = {
+  //   test(multiple(6): _*)
+  // }
+
+  // @Test def multiple_FJScheduler(): Unit = {
+  //   test(multiple(8): _*)
+  // }
+
+  // // last rerun allTransitions with the real NSC Interpreter ...
+
+  // @Ignore("See description of failure in class head.")
+  // @Test def allTransitions_RealNSC(): Unit = {
+  //   test(allTransitions)
+  // }
+}
+
+object EnsimeReplTest {
+  import TestFutureUtil._
+
+  def test(ses: Seq[Expect]*): Unit = {
+    val rs = ses map { new Recorder(_) }
+    whenReady(tryAsResult(Future.sequence {
+      val reqs = rs map { _.sendRequests() }
+      reqs
+    })) { ignore =>
+      rs foreach { _.assertMatches() }
+    }
+  }
+
+  val TheOneException = new RuntimeException("TheOne") {
+    // suppress the useless stack traces...
+    setStackTrace(Array.empty[StackTraceElement])
+  }
+
+  trait Expect
+  def Expect(es: Expect*): Seq[Expect] = es
+  def replace(es: Seq[Expect], f: PartialFunction[Expect, Expect]): Seq[Expect] =
+    for (e <- es) yield if (f.isDefinedAt(e)) f(e) else e
+
+  trait Request extends Expect { def msg: Any }
+  case class init(msg: Init) extends Request
+  case object stop extends Request { val msg = Stop }
+  case class exec(msg: Exec) extends Request
+  case object drop extends Request { val msg = Drop }
+  case object quit extends Request { val msg = Quit }
+  case class bad(msg: Any) extends Request
+
+  trait Reply extends Expect
+  case class Starting(init: Init) extends Reply
+  case class Started(init: Init) extends Reply
+  case object Stopped extends Reply
+  case object Replaying extends Reply
+  case object Replayed extends Reply
+  case object Dropped extends Reply
+  case class Added(exec: Exec) extends Reply
+  case class Doing(exec: Exec) extends Reply
+  case class Done(exec: Exec, result: Result, output: String) extends Reply
+  case object Terminating extends Reply
+  case class Failed(request: Any, thrown: Throwable, output: String) extends Reply
+  case class Unknown(request: Any) extends Reply
+
+  def quitFromZ = Expect(quit, Terminating)
+  def quitFromH = Expect(quit, Dropped, Terminating)
+  def quitFromB = Expect(quit, Stopped, Dropped, Terminating)
+
+  def execAdded(line: Exec) =
+    Expect(exec(line), Added(line))
+
+  def DoingFailed(line: Exec) =
+    Expect(Doing(line), Failed(line, TheOneException, ""))
+
+  case class InOut(in: String, out: (Int => String)) {
+    def execAdded = EnsimeReplTest.execAdded(in)
+    def DoingFailed = EnsimeReplTest.DoingFailed(in)
+    def execFailed = execAdded ++ DoingFailed
+
+    def DoingDone(n: Int) =
+      Expect(Doing(in), Done(in, Success, out(n)))
+
+    def execDone(n: Int) =
+      execAdded ++ DoingDone(n)
+  }
+
+  trait Initialization {
+    def settings: Init
+
+    def initStarting =
+      Expect(init(settings), Starting(settings))
+
+    def initStarted =
+      initStarting ++ Expect(Started(settings))
+
+    def initReplaying =
+      initStarting ++ Expect(Replaying)
+
+    def ReplayedStarted =
+      Expect(Replayed) ++ Expect(Started(settings))
+
+    def initFailed =
+      initStarting ++ Expect(Failed(settings, TheOneException, ""))
+
+    def initReplay(ios: InOut*): Seq[Expect] =
+      initReplaying ++
+        ios.zipWithIndex.map { t => t._1.DoingDone(t._2) }.flatten ++
+        ReplayedStarted
+
+    def initReplayingFailed(io: InOut) =
+      initReplaying ++ blame(io.DoingFailed)
+
+    def blame(es: Seq[Expect]) =
+      replace(es, { case Failed(r, t, o) => Failed(settings, t, o) })
+
+    def steal(es: Seq[Expect]) =
+      replace(es, {
+        case init(_) => init(settings)
+        case Starting(_) => Starting(settings)
+        case Started(_) => Started(settings)
+        case Failed(i: Init, t, o) => Failed(settings, t, o)
+      })
+  }
+
+  object Unspecified extends Initialization {
+    val settings = new Init { override def toString = "Unspecified" }
+  }
+  def initStarting = Unspecified.initStarting
+  def initStarted = Unspecified.initStarted
+  def initReplaying = Unspecified.initReplaying
+  def ReplayedStarted = Unspecified.ReplayedStarted
+  def initFailed = Unspecified.initFailed
+  def initReplay(ios: InOut*): Seq[Expect] = Unspecified.initReplay(ios: _*)
+  def initReplayingFailed(io: InOut) = Unspecified.initReplayingFailed(io)
+
+  object Echopreter extends Initialization with Interpreter {
+    val settings = new Init { override def toString = "Echopreter" }
+    // just echo back each line of code as if that were the result
+    def interpret(e: String) = { print(e); Success }
+
+    def editOutput(es: Seq[Expect]) =
+      replace(es, {
+        case Done(e, r, _) => Done(e, r, e)
+      })
+
+    override def steal(es: Seq[Expect]) =
+      super.steal(editOutput(es))
+  }
+  object Failpreter extends Initialization with Interpreter {
+    def interpret(e: String) = throw TheOneException
+    val settings = new Init { override def toString = "Failpreter" }
+    override def steal(es: Seq[Expect]) =
+      throw new UnsupportedOperationException()
+  }
+  object Failder extends Initialization {
+    val settings = new Init { override def toString = "Failder" }
+    override def steal(es: Seq[Expect]) =
+      throw new UnsupportedOperationException()
+  }
+
+  object TestBuilder extends Builder {
+    def interpreter(i: Init) =
+      if (i eq Echopreter.settings) Echopreter
+      else if (i eq Failpreter.settings) Failpreter
+      else if (i eq Failder.settings) throw TheOneException
+      else DefaultBuilder.interpreter(i)
+  }
+
+  def messageToRequest(msg: Any): Request =
+    msg match {
+      case i: Init => init(i)
+      case Stop => stop
+      case e: Exec => exec(e)
+      case Drop => drop
+      case Quit => quit
+      case x => bad(x)
+    }
+
+  def filterRequests(es: Seq[Expect]): Seq[Request] =
+    es filter { _.isInstanceOf[Request] } map { _.asInstanceOf[Request] }
+
+  def filterReplies(es: Seq[Expect]): Seq[Reply] =
+    es filter { _.isInstanceOf[Reply] } map { _.asInstanceOf[Reply] }
+
+  class Recorder(val expected: Seq[Expect]) {
+    private val buffer = new collection.mutable.ListBuffer[Expect]
+    def add(e: Expect): Unit = { synchronized { buffer += e } }
+    def record = { synchronized { buffer.toList } }
+
+    val client = new Client {
+      override def starting(i: Init): Unit = { add(Starting(i)) }
+      override def started(i: Init): Unit = { add(Started(i)) }
+      override def stopped(): Unit = { add(Stopped) }
+      override def replaying(): Unit = { add(Replaying) }
+      override def replayed(): Unit = { add(Replayed) }
+      override def dropped(): Unit = { add(Dropped) }
+      override def added(e: Exec): Unit = { add(Added(e)) }
+      override def doing(e: Exec): Unit = { add(Doing(e)) }
+      override def done(e: Exec, r: Result, o: String): Unit = { add(Done(e, r, o)) }
+      override def terminating(): Unit = { add(Terminating) }
+      override def failed(r: Any, t: Throwable, o: String): Unit = { add(Failed(r, t, o)) }
+      override def unknown(request: Any): Unit = { add(Unknown(request)) }
+    }
+
+    val tested = new EnsimeRepl(client, TestBuilder)
+
+    def sendRequests(): Future[Unit] = {
+      def chainFutures(seq: Seq[Request]): Future[Unit] =
+        if (seq.isEmpty)
+          Future.successful {}
+        else
+          dispatch(seq.head.msg).flatMap { unit =>
+            chainFutures(seq.tail)
+          }.recoverWith {
+            case ignoreAllExceptions =>
+              if (seq.drop(1).nonEmpty)
+                chainFutures(seq.drop(1).tail)
+              else chainFutures(Nil)
+          }
+      chainFutures(filterRequests(expected))
+    }
+
+    private def dispatch(msg: Any): Future[Unit] = {
+      add(messageToRequest(msg))
+      msg match {
+        case e: Exec => tested.exec(e)
+        case i: Init => tested.init(i)
+        case Stop => tested.stop()
+        case Drop => tested.drop()
+        case Quit => tested.quit()
+        case un => tested.unknown(un)
+      }
+    }
+
+    def assertMatches(): Unit = {
+      val actual = record
+      val expectedRequests = filterRequests(expected).toList
+      val actualRequests = filterRequests(actual).toList
+      val expectedReplies = filterReplies(expected).toList
+      val actualReplies = filterReplies(actual).toList
+      //assertEquals("requests", expectedRequests, actualRequests)
+      //assertEquals("replies", expectedReplies, actualReplies)
+    }
+  }
+}
+
+object TestFutureUtil {
+  import scala.concurrent.ExecutionContext.Implicits.global
+  import scala.concurrent.duration._
+  val DefaultTimeout = 1000 millis
+
+  def whenReady[T](future: => Future[T], timeout: Duration = DefaultTimeout)(withal: T => Unit): Unit =
+    withal(Await.result(future, timeout))
+
+  def tryAsResult[T](future: => Future[T]): Future[Try[T]] = {
+    val p = Promise[Try[T]]
+    future onComplete { t =>
+      p success (t);
+    }
+    p.future
+  }
+
+  // def waitForConditionOrTimeout(cond: => Boolean, timeout: Duration = DefaultTimeout): Unit = {
+  //   Try(Assert.assertTrue(Await.result(Future { cond }, timeout))) recover {
+  //     case all => Assert.fail(s"condition '$cond' timed out")
+  //   }
+  // }
+}
+
+trait EnsimeReplTestUtils extends Assertions {
+  import scala.concurrent.ExecutionContext.Implicits.global
+  import scala.concurrent.duration._
+  val DefaultTimeout = 1000 millis
+
+  def whenReady[T](future: => Future[T], timeout: Duration = DefaultTimeout)(withal: T => Unit): Unit =
+    withal(Await.result(future, timeout))
+
+  def tryAsResult[T](future: => Future[T]): Future[Try[T]] = {
+    val p = Promise[Try[T]]
+    future onComplete { t =>
+      p success (t);
+    }
+    p.future
+  }
+
+  // def waitForConditionOrTimeout(cond: => Boolean, timeout: Duration = DefaultTimeout): Unit = {
+  //   Try(Assert.assertTrue(Await.result(Future { cond }, timeout))) recover {
+  //     case all => Assert.fail(s"condition '$cond' timed out")
+  //   }
+  // }
+
+}
---
+++ core/src/main/scala/org/ensime/core/repl/EnsimeRepl.scala
@@ -0,0 +1,271 @@
+// Copyright: 2010 - 2016 https://github.com/ensime/ensime-server/graphs
+// Licence: http://www.gnu.org/licenses/gpl-3.0.en.html
+package org.ensime.core.repl
+
+import java.util.concurrent.CopyOnWriteArrayList
+import java.util.concurrent.atomic.AtomicReference
+
+import scala.concurrent.Future
+import scala.tools.nsc.Settings
+import scala.tools.nsc.interpreter.Results.Result
+
+import EnsimeRepl._
+
+/**
+ * An `EnsimeRepl` is a simple Finite State Machine with 4 states based on
+ * whether the REPL is running/not and whether the history `isEmpty`/not.
+ *   - Z = {stopped,isEmpty} (the zero state)
+ *   - R = {started,isEmpty} (running, but that's all)
+ *   - H = {stopped,history} (history, but that's all)
+ *   - B = {started,history} (the both state)
+ *
+ * Each of these transition requests affects at most one of the two axes:
+ *   - `Init` and `Stop` affect the running/not axis.
+ *   - `Exec` and `Drop` affect the history contents.
+ * The `Quit` request can affect both axes.
+ *
+ * The actions performed during state transitions are composed of calls to
+ * these six primitives (which are private methods on the `EclipsRepl`):
+ *   - `boot` and `halt` start/stop the REPL
+ *   - `add` and `zap` alter the history
+ *   - `doit` feeds a line of code to the interpreter
+ *   - `redo` calls `doit` for each line of code in the history
+ */
+object EnsimeRepl {
+  /**
+   * Requests the REPL start over again using the given settings. Calls `halt`
+   * if needed, then calls `boot`, which calls `redo` if there is any history.
+   * {{{ Init: (R{halt}|Z)->{boot}->R. (B{halt}|H)->{boot;redo}->B. }}}
+   * WARNING: `Settings` will be changed to `ImmutableSettings` ASAP.
+   * Until then it is up to the caller to avoid race conditions (e.g. by
+   * copying the settings before sending and never changing them after).
+   */
+  type Init = Settings //TODO when ImmutableSettings gets written...
+
+  /**
+   * Requests the REPL shut down, allowing it to be garbage collected.
+   * {{{ Stop: Z->Z. H->H. R->{halt}->Z. B->{halt}->H. }}}
+   */
+  val Stop = new Object { override def toString = "Stop" }
+
+  /**
+   * Requests the REPL run a line of code (now if running, or later as
+   * part of replaying history if not).
+   * Always calls `add`, which calls `doit` if the REPL is running.
+   * {{{ Exec: (Z|H)->{add}->H. (R|B)->{add;doit}->B. }}}
+   */
+  type Exec = String
+
+  /**
+   * Requests the REPL history be erased.
+   * {{{ Drop: Z->Z. R->R. H->{zap}->Z. B->{zap}->R. }}}
+   */
+  val Drop = new Object { override def toString = "Drop" }
+
+  /**
+   * Performs `Stop` and `Drop`, then de-schedules the `Future`, allowing
+   * it to be garbage collected.
+   */
+  val Quit = new Object { override def toString = "Quit" }
+
+  /**
+   * Almost a `Listener`. Status calls are dispatched on the `EnsimeRepl` `Future`'s thread,
+   * so implementors must be careful to avoid race conditions. If the `Client`
+   * is SWT-based it must use something like `SWTUtils.asyncExec`.
+   */
+  trait Client {
+    /** Called just before initialization of a new Interpreter. */
+    def starting(init: Init): Unit = {} // after `halt` on entry to `boot` : `Init`
+
+    /** Called after successful initialization of a new Interpreter. */
+    def started(init: Init): Unit = {} // at successful exit from `boot` : `Init`
+
+    /** Called whenever an Interpreter is stopped. */
+    def stopped(): Unit = {} // by `halt` : `Init`, `Stop`, `Quit`
+
+    /** Called after initialization before replaying of the history. */
+    def replaying(): Unit = {} // on entry to `redo` : `Init`
+
+    /** Called upon completion of the replaying of the history. */
+    def replayed(): Unit = {} // at exit from `redo` : `Init`
+
+    /** Called whenever the history is cleared. */
+    def dropped(): Unit = {} // by `zap` : `Drop`, `Quit`
+
+    /** Called when a line of code is added to the history. */
+    def added(exec: Exec): Unit = {} // by `add` : `Exec`
+
+    /** Called just before the interpretation of a line of code. */
+    def doing(exec: Exec): Unit = {} // on entry to `doit` : `Init`, `Exec`
+
+    /** Called after interpretation of a line of code. */
+    def done(exec: Exec, result: Result, output: String): Unit = {}
+    // at exit from `doit` : `Init`, `Exec`
+
+    /** Called just before the `Future` enters its `Terminated` state. */
+    def terminating(): Unit = {} // at exit from the handler for `Quit`, in catch
+
+    /** Called for any unrecognized message. */
+    def unknown(request: Any): Unit = {}
+
+    /**
+     * Called when something is thrown. The `EnsimeRepl` `Future` responds to any exception
+     * by behaving as if `Quit` had been called.
+     *
+     * `boot` can fail if the settings are bad.
+     * None of the other actions should fail under normal circumstances.
+     * For example, the interpreter catches exceptions and returns a
+     * `Result` of type `Error`, which `doit` reports via `done`.
+     *
+     * @param request the `Init`, `Exec`, etc. message
+     */
+    def failed(request: Any, thrown: Throwable, output: String): Unit = {}
+  }
+
+  // implementation note: request and status messages were originally like:
+  //   trait Request ; trait Status
+  //   case class Init(settings:Settings) extends Request
+  //   case object Stop extends Request
+  //   case class Starting(init:Init) extends Status
+  //   case object Stopped extends Status
+  // etc., etc., with `type Client = (Status => Unit)`
+  // it was all very pretty, but the resulting bytecode was too bloated.
+
+  /** The API we use from the Scala Interpreter. @see Builder */
+  trait Interpreter {
+    /** prints the output to `Console.out` */
+    def interpret(e: Exec): Result
+  }
+
+  /** Allows plugging in alternate (i.e. test) `Interpreter`s. */
+  trait Builder {
+    /** Called by `EnsimeRepl` constructor, calls its `Worker.start`. */
+    def constructed(a: EnsimeRepl): Unit = {}
+
+    /** Returns a new `Interpreter`. */
+    def interpreter(i: Init): Interpreter
+  }
+
+  /** Returns instances of the Scala Interpreter. */
+  object DefaultBuilder extends Builder {
+    def interpreter(i: Init) = new Interpreter {
+      val intp = new scala.tools.nsc.interpreter.IMain(i)
+      intp.initializeSynchronous()
+
+      def interpret(e: Exec) = {
+        val r = intp.interpret(e)
+        intp.reporter.flush()
+        r
+      }
+    }
+  }
+}
+
+/**
+ * Wraps a Scala interpreter in an `Future` so it can work on a background
+ * thread. Because the results from executing a line of code depend upon the
+ * previous lines, each `EnsimeRepl` should be used only by a single entity.
+ * Typically that entity is the `Client` passed to the constructor.
+ *
+ * These five convenience methods are available: `init`, `exec`, `drop`,
+ * `stop`, and `quit`. All they do is send the corresponding request message.
+ */
+class EnsimeRepl(client: Client, builder: Builder) {
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  def this(client: Client) = this(client, DefaultBuilder)
+
+  def init(settings: Init): Future[Unit] = safely(boot, settings)
+  def exec(line: Exec): Future[Unit] = safely(add, line)
+  def drop(): Future[Unit] = safely(zap, Drop)
+  def stop(): Future[Unit] = safely(halt, Stop)
+  def quit(): Future[Unit] = safely(term(notifyClient = true), Quit)
+  def unknown(un: Any): Future[Unit] = safely(unkn, un)
+
+  import java.io.{ ByteArrayOutputStream => BAOS }
+
+  private val intpRef: AtomicReference[Interpreter] = new AtomicReference
+  private def intp = intpRef.get
+
+  private def unkn(un: Any, b: BAOS): Unit =
+    client.unknown(un)
+
+  private def boot(i: Init, b: BAOS): Unit = {
+    halt(i, b)
+    client.starting(i)
+    intpRef.getAndSet(builder.interpreter(i))
+    intp.getClass // throw new NullPointerException
+    redo(b)
+    client.started(i)
+  }
+
+  private def halt(r: Any, b: BAOS): Unit = {
+    Option(intpRef.getAndSet(null)).foreach { _ =>
+      client.stopped()
+    }
+  }
+
+  import scala.collection.JavaConverters._
+  private val hist = (new CopyOnWriteArrayList[Exec]).asScala
+
+  private def add(e: Exec, b: BAOS): Unit = {
+    hist += e
+    client.added(e)
+    doit(e, b)
+  }
+
+  private def zap(r: Any, b: BAOS): Unit = {
+    if (!hist.isEmpty) {
+      hist.clear()
+      client.dropped()
+    }
+  }
+
+  private def redo(b: BAOS): Unit = {
+    if (!hist.isEmpty) {
+      client.replaying()
+      hist foreach { doit(_, b) }
+      client.replayed()
+    }
+  }
+
+  private def doit(e: Exec, b: BAOS): Unit = {
+    Option(intp).map { interpreter =>
+      client.doing(e)
+      val r = interpreter.interpret(e)
+      val o = b.toString.trim; b.reset()
+      client.done(e, r, o)
+    }.orElse {
+      //logger.debug("interpreter not found")
+      None
+    }
+  }
+
+  private def term(notifyClient: Boolean)(r: Any, b: BAOS): Unit = {
+    halt(r, b)
+    zap(r, b)
+    if (notifyClient)
+      client.terminating()
+  }
+
+  private def safely[T](work: (T, BAOS) => Unit, r: T): Future[Unit] = Future {
+    val b = new BAOS
+    Console.withOut(b) {
+      Console.withErr(b) {
+        try {
+          work(r, b)
+        } catch {
+          case t: Throwable =>
+            try {
+              val o = b.toString.trim; b.reset()
+              client.failed(r, t, o)
+              term(notifyClient = false)(r, b)
+            } finally "sdasdfasdff"
+          //logger.error("error during command processing", t)
+        }
+      }
+    }
+  }
+
+  builder.constructed(this)
+}
